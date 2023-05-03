79a89b9bd7318f1d07b92097a6ed772f5923482f
Joseph Freeston <snorklerjoe@gmail.com>
2023-05-02 21:12:55 -0400

    Posts all beacon messages continuously

--- code.py
+++ code.py
@@ -7,3 +7,18 @@ Intended operation:
 - Otherwise listen continuously for beacon messages and post them immediately
 - Check for code updates once every hour
 """
+
+import servercom
+from servercom import irrx
+import time
+
+# Set up the server communication object
+a=servercom.Connection()
+
+while True:
+    if time.time() % 1000 == 0:
+        a.code_update()
+    msg=irrx.receive()
+    if msg:
+        a.post(servercom.BeaconChallenge(msg.text))
+    time.sleep(0.1)
